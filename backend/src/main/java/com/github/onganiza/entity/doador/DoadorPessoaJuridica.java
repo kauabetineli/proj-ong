20417cd91a9c9de6bd13c8ce452de726ecd7af44
Kauã <kauaikoto@gmail.com>
2025-06-22 11:07:49 -0300

    fix: Doadores com CPF/CNPJ unicos

--- backend/src/main/java/com/github/onganiza/entity/doador/DoadorPessoaJuridica.java
+++ backend/src/main/java/com/github/onganiza/entity/doador/DoadorPessoaJuridica.java
@@ -1,13 +1,18 @@
 package com.github.onganiza.entity.doador;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CNPJ;
 
 @Entity
 @Getter
 @Setter
 public class DoadorPessoaJuridica extends Doador {
     private String razaoSocial;
+
+    @CNPJ
+    @Column(unique = true)
     private String cnpj;
 }
